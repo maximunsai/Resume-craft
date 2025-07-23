c7fcb432c45610938016229ada3abefdc708a30f
Saikiran Nagulapuram <saikiran.nagulapuram@thinkartha.com>
2025-07-23 15:10:37 +0530

    Fix: pdfFont.ts file

--- src/lib/pdf-fonts.ts
+++ src/lib/pdf-fonts.ts
@@ -13,87 +13,128 @@ const getFontPath = (fontFile: string) => {
 
 // This function will register all fonts needed for all PDF templates, but only once.
 export const registerAllPdfFonts = () => {
-    if (fontsRegistered) {
-        return;
-    }
+  if (fontsRegistered) return;
 
-    console.log("Registering all PDF fonts globally from their absolute paths...");
-    
-    try {
-        // --- This list contains all the fonts we have used so far ---
-        Font.register({
-            family: 'Helvetica', fonts: [
-                { src: getFontPath('Helvetica.ttf') },
-                { src: getFontPath('Helvetica-Bold.ttf'), fontWeight: 'bold' },
-            ]
-        });
-        Font.register({
-            family: 'Times New Roman', fonts: [
-                { src: getFontPath('times-new-roman.ttf') },
-                { src: getFontPath('times-new-roman-bold.ttf'), fontWeight: 'bold' },
-                { src: getFontPath('times-new-roman-italic.ttf'), fontStyle: 'italic' },
-            ]
-        });
-        Font.register({
-            family: 'Lato', fonts: [
-                { src: getFontPath('lato-regular.ttf') },
-                { src: getFontPath('lato-bold.ttf'), fontWeight: 'bold' },
-                { src: getFontPath('lato-black.ttf'), fontWeight: 'heavy' },
-            ]
-        });
-        Font.register({
-            family: 'Montserrat', fonts: [
-                { src: getFontPath('montserrat-regular.ttf') },
-                { src: getFontPath('montserrat-bold.ttf'), fontWeight: 'bold' },
-                { src: getFontPath('montserrat-semibold.ttf'), fontWeight: 'semibold' },
-            ]
-        });
-        Font.register({
-            family: 'CMU Serif', fonts: [
-                { src: getFontPath('cmu-serif-roman.ttf') },
-                { src: getFontPath('cmu-serif-bold.ttf'), fontWeight: 'bold' },
-            ]
-        });
-        Font.register({
-            family: 'Roboto', fonts: [
-                { src: getFontPath('roboto-regular-webfont.ttf') },
-                { src: getFontPath('roboto-bold-webfont.ttf'), fontWeight: 'bold' }
-            ]
-        });
-        Font.register({ family: 'Roboto Mono', src: getFontPath('robotomono-regular-webfont.ttf') });
-        Font.register({ family: 'Oswald', src: getFontPath('oswald-regular.ttf') });
-        Font.register({ family: 'Oswald-Bold', src: getFontPath('oswald-bold.ttf') });
-        Font.register({
-            family: 'Playfair Display', fonts: [
-                { src: getFontPath('playfair-display-latin-400-normal.woff') },
-                { src: getFontPath('playfair-display-latin-700-normal.woff'), fontWeight: 'bold' },
-                { src: getFontPath('playfair-display-latin-400-italic.woff'), fontStyle: 'italic' },
-            ]
-        });
-        Font.register({
-            family: 'Garamond', fonts: [
-                { src: getFontPath('EBGaramond-Regular.ttf') },
-                { src: getFontPath('EBGaramond-Bold.ttf'), fontWeight: 'bold' }
-            ]
-        });
-        Font.register({
-            family: 'Inter', fonts: [
-                { src: getFontPath('Inter-Regular.woff') },
-                { src: getFontPath('Inter-Bold.woff'), fontWeight: 'bold' }
-            ]
-        });
-        Font.register({
-            family: 'Raleway', fonts: [
-                { src: getFontPath('raleway-latin-400-normal.woff') },
-                { src: getFontPath('raleway-latin-700-normal.woff'), fontWeight: 'bold' },
-                { src: getFontPath('raleway-latin-900-normal.woff'), fontWeight: 'heavy' }
-            ]
-        });
+  console.log("Registering all PDF fonts globally from their absolute paths...");
 
-        fontsRegistered = true;
-        console.log("All local PDF fonts successfully registered.");
+  try {
+    // Helvetica (Modernist, Corporate, Simple)
+    Font.register({
+      family: 'Helvetica',
+      fonts: [
+        { src: getFontPath('Helvetica.ttf') },
+        { src: getFontPath('Helvetica-Bold.ttf'), fontWeight: 'bold' },
+      ],
+    });
 
-    } catch (error) {
-        console.error("CRITICAL: FONT REGISTRATION FAILED.", error);
-    }
+    // Times New Roman (Classic)
+    Font.register({
+      family: 'Times New Roman',
+      fonts: [
+        { src: getFontPath('times-new-roman.ttf') },
+        { src: getFontPath('times-new-roman-bold.ttf'), fontWeight: 'bold' },
+        { src: getFontPath('times-new-roman-italic.ttf'), fontStyle: 'italic' },
+      ],
+    });
+
+    // Lato (Executive, Apex)
+    Font.register({
+      family: 'Lato',
+      fonts: [
+        { src: getFontPath('lato-regular.ttf')},
+        { src: getFontPath('lato-bold.ttf'), fontWeight: 'bold' },
+        // { src: getFontPath('lato-black.ttf'), fontWeight: '900' },
+      ],
+    });
+
+    // Montserrat (Creative, Pinnacle)
+    Font.register({
+      family: 'Montserrat',
+      fonts: [
+        { src: getFontPath('montserrat-regular.ttf') },
+        { src: getFontPath('montserrat-bold.ttf'), fontWeight: 'bold' },
+        // { src: getFontPath('montserrat-semibold.ttf'), fontWeight: '600' }, // semibold numeric
+      ],
+    });
+
+    // CMU Serif (Academic)
+    Font.register({
+      family: 'CMU Serif',
+      fonts: [
+        { src: getFontPath('cmu-serif-roman.ttf') },
+        { src: getFontPath('cmu-serif-bold.ttf'), fontWeight: 'bold' },
+      ],
+    });
+
+    // Roboto (Technical)
+    Font.register({
+      family: 'Roboto',
+      fonts: [
+        { src: getFontPath('roboto-regular-webfont.ttf') },
+        { src: getFontPath('roboto-bold-webfont.ttf'), fontWeight: 'bold' },
+      ],
+    });
+
+    Font.register({
+      family: 'Roboto Mono',
+      src: getFontPath('robotomono-regular-webfont.ttf'),
+    //   fontWeight: '400',
+    });
+
+    // Oswald (Bold)
+    Font.register({
+      family: 'Oswald',
+      src: getFontPath('oswald-regular.ttf'),
+    //   fontWeight: '400',
+    });
+    Font.register({
+      family: 'Oswald-Bold',
+      src: getFontPath('oswald-bold.ttf'),
+      fontWeight: 'bold',
+    });
+
+    // Playfair Display (Elegant)
+    // Convert woff -> ttf if possible, using .ttf here is a must
+    Font.register({
+      family: 'Playfair Display',
+      fonts: [
+        { src: getFontPath('playfair-display-latin-400-normal.ttf') },
+        { src: getFontPath('playfair-display-latin-700-normal.ttf'), fontWeight: 'bold' },
+        { src: getFontPath('playfair-display-latin-400-italic.ttf'), fontStyle: 'italic' },
+      ],
+    });
+
+    // Garamond (Cascade)
+    Font.register({
+      family: 'Garamond',
+      fonts: [
+        { src: getFontPath('EBGaramond-Regular.ttf')},
+        { src: getFontPath('EBGaramond-Bold.ttf'), fontWeight: 'bold' },
+      ],
+    });
+
+    // Inter (Metro)
+    Font.register({
+      family: 'Inter',
+      fonts: [
+        { src: getFontPath('inter-Regular.ttf') },
+        { src: getFontPath('inter-Bold.ttf'), fontWeight: 'bold' },
+      ],
+    });
+
+    // Raleway (Cosmopolitan)
+    Font.register({
+      family: 'Raleway',
+      fonts: [
+        { src: getFontPath('raleway-latin-400.woff') },
+        { src: getFontPath('raleway-latin-900.woff'), fontWeight: 'bold' },
+        // { src: getFontPath('raleway-black.ttf'), fontWeight: '900' },
+      ],
+    });
+
+    fontsRegistered = true;
+    console.log("All PDF fonts successfully registered!");
+  } catch (error) {
+    console.error("CRITICAL ERROR during PDF fonts registration:", error);
+  }
 };
